87b2920cee8b51811580bc5eadd7cc17556d7f30
Tommy Hui <aesops0124@gmail.com>
2018-12-09 11:51:45 +0800

    init mobile ant design

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'antd/dist/antd.css';
+import 'antd-mobile/dist/antd-mobile.css';
 import {BrowserRouter as Router} from 'react-router-dom'
 
 // ReactDOM.render(<App />, document.getElementById('root'));
