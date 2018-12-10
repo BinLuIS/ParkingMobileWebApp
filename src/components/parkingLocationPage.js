52b62aaeb4b777fb9c2765b1853d281f57464f18
Tommy Hui <aesops0124@gmail.com>
2018-12-10 14:48:51 +0800

    [Tommy] Add parkingLocationPage

---
+++ src/components/parkingLocationPage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import '../css/parkingLocationPage.css';
+
+const styles = theme => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
+function ListDividers(props) {
+  const { classes } = props;
+  return (
+    <div>
+        <div class="am-list-header"><span><h1 style={{textAlign: "center",color: "white"}}>停車地點</h1></span></div>
+        <List component="nav" className={classes.root}>
+        <ListItem button>
+            <ListItemText primary="停車場A (10)" />
+        </ListItem>
+        <Divider />
+        <ListItem button divider>
+            <ListItemText primary="停車場B (10)" />
+        </ListItem>
+        <ListItem button>
+            <ListItemText primary="停車場C (10)" />
+        </ListItem>
+        <Divider light />
+        </List>
+    </div> 
+  );
+}
+
+ListDividers.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ListDividers)
--- src/components/pickCarPage.js
+++ src/components/pickCarPage.js
@@ -1,30 +1,55 @@
-import React, { Component } from 'react';
-import { List, InputItem, WhiteSpace, Button } from 'antd-mobile';
+// import React, { Component } from 'react';
+import {WhiteSpace, Button } from 'antd-mobile';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import '../css/pickCarPage.css';
+
+// export default class pickCarPage extends Component {
+//     render() {
+//         return (
+            
 
 
-export default class pickCarPage extends Component {
-    render() {
-        return (
-            <div>
-                <List renderHeader={() => <span><h1 style={{ textAlign: "center", color: "white" }}>停車地點</h1></span>}>
-                    <div style={{ padding: '0 15px', display: "flex", justifyContent: 'space-between' }}>
-                        <div style={{ display: 'flex', padding: '20px 0' }}>
-                            <div><h2>選擇停車場</h2></div>
-                        </div>
-                        <Button type="primary" inline size="medium" style={{ marginRight: '4px', marginTop: '25px' }}>選擇</Button>
-                    </div>
-                </List>
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <div>
-                    <Button type="primary" >完成訂單</Button><WhiteSpace />
-                </div>
-            </div>
-
-
-        );
-    }
-}
+//         );
+//     }
+// }
+const styles = theme => ({
+    root: {
+      backgroundColor: theme.palette.background.paper,
+    },
+  });
+  
+  function ListDividers(props) {
+    const { classes } = props;
+    return (
+    <div>
+        <div class="am-list-header"><span><h1 style={{textAlign: "center",color: "white"}}>停車地點</h1></span></div>
+	    <List>
+            <List component="nav" className={classes.root}>
+                <ListItem button>
+                    <ListItemText primary="選擇停車場" />
+                </ListItem>
+            </List>
+        </List>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div>
+            <Button type="primary" >完成訂單</Button><WhiteSpace />
+        </div>
+    </div>
+    );
+  }
+  
+  ListDividers.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(ListDividers)
