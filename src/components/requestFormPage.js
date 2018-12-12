0cf91c0d7570ca8e55bb6ecd4a675233aa7b462c
Tommy Hui <aesops0124@gmail.com>
2018-12-13 02:00:13 +0800

    [Tommy] Add Advertisements

--- src/components/requestFormPage.js
+++ src/components/requestFormPage.js
@@ -5,6 +5,11 @@ import SloganPage from './sloganPage';
 
 export default class requestFormPage extends Component {
   
+  state = {
+    data: ['1', '2', '3'],
+    imgHeight: 176,
+    slideIndex: 2,
+  }
   onAdded = () => {
     const {name,carnum} = this.refs
     console.log(carnum)
@@ -30,6 +35,54 @@ export default class requestFormPage extends Component {
     carnum.setState({value: ''});
     name.setState({value: ''});
   }
+  
+  componentDidMount() {
+    // simulate img loading
+    setTimeout(() => {
+      this.setState({
+        data: ['https://holland.pk/uptow/i4/9c889af0fae008bac83a304525b4398b.jpg', 'https://holland.pk/uptow/i4/01ba5299072c7a0d9a19ebf4f151d65e.jpg'],
+      });
+    }, 100);
+  }
+  getAdvertisement = () => {
+    return (
+      <WingBlank>
+        
+        <WhiteSpace />
+        <Carousel
+          autoplay={true}
+          dots={false}
+          infinite
+          selectedIndex={this.state.slideIndex}
+          beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+          afterChange={index => console.log('slide to', index)}
+          autoplayInterval={5000}
+        >
+          {this.state.data.map((val, index) => (
+            
+            <div
+              key={val + index}
+              href="http://www.alipay.com"
+              style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight, float:"right"  }}
+            >
+            {/* https://zos.alipayobjects.com/rmsportal/AiyWuByWklrrUDlFignR.png */}
+            {/* require('../icon/caricon.png') */}
+            
+              <img
+                src={val}
+                alt=""
+                style={{ width: '50%', verticalAlign: 'top'}}
+                onLoad={() => {
+                  // fire window resize event to change height
+                  window.dispatchEvent(new Event('resize'));
+                  this.setState({ imgHeight: 'auto' });
+                }}
+              />
+            </div>
+          ))}
+        </Carousel>
+      </WingBlank>);
+  }
   render() {
 
     return (
@@ -49,6 +102,7 @@ export default class requestFormPage extends Component {
             <Button type="primary" onClick={this.onFetch}>取車</Button><WhiteSpace />
             <Button type="primary" onClick={this.onInquire}>查詢</Button><WhiteSpace />
           </div>
+          {this.getAdvertisement()}
         </div>
     );
   }
